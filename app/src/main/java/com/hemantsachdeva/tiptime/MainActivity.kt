7d9146be45c9881f12c0a5901b127d5ecb829348
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-04 12:14:01 +0530

    Round up the tip
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/hemantsachdeva/tiptime/MainActivity.kt
+++ app/src/main/java/com/hemantsachdeva/tiptime/MainActivity.kt
@@ -26,5 +26,11 @@ class MainActivity : AppCompatActivity() {
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
     }
 }
