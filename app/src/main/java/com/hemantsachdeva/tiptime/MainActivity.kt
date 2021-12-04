3d28913715ed32ef94bfd835f250c29715140892
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-04 12:45:33 +0530

    treewide: Code quality improvements
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/hemantsachdeva/tiptime/MainActivity.kt
+++ app/src/main/java/com/hemantsachdeva/tiptime/MainActivity.kt
@@ -21,7 +21,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        // If the cost is null or 0, then display 0 tip and exit this function early.
+        if (cost == null || cost == 0.0) {
+            binding.tipResult.text = ""
+            return
+        }
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
@@ -34,6 +40,11 @@ class MainActivity : AppCompatActivity() {
             tip = kotlin.math.ceil(tip)
         }
 
+        // Display the formatted tip value on screen
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip : Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
