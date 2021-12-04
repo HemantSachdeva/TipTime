4b8af77feb65b4b133fe49116c5ee2de2ff00b77
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-04 12:08:59 +0530

    Get the tip percentage
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/hemantsachdeva/tiptime/MainActivity.kt
+++ app/src/main/java/com/hemantsachdeva/tiptime/MainActivity.kt
@@ -21,5 +21,10 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
     }
 }
